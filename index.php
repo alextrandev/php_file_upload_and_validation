98d67fafcccd6568366f532b1c779afa17c9cbef
dk10119 <tranduclong092@gmail.com>
2024-04-11 11:19:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File upload and validation</title>
+</head>
+
+<body>
+    <form action="">
+        <table>
+            <tr>
+                <td><label for="file">Select a file:</label></td>
+                <td><input type="file" name="file" id="file"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
