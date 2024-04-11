36a2229973461b285e0c8c007f46f2e87c421dc1
dk10119 <tranduclong092@gmail.com>
2024-04-11 17:26:05 +0300

    add a try catch block

--- file_input_output.php
+++ file_input_output.php
@@ -2,3 +2,7 @@
 //show all files in a directory
 $dir_content = scandir("./uploads_copy");
 echo "<pre>", print_r($dir_content), "</pre>";
+
+//print the content of a text file
+$file = file_get_contents('./uploads_copy/note.md');
+echo "<pre>", $file, "</pre>";
--- index.php
+++ index.php
@@ -1,45 +1,52 @@
 <?php
 if (isset($_POST['file_upload'])) {
-    $fileName = $_FILES['sample_file']['name'];
-    $filePath = $_FILES['sample_file']['tmp_name']; //get the path of the file
-    $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION); // get extension
-    $newFileName = time() . "." . $fileExtension; //change file name
-    $fileSize = $_FILES['sample_file']['size'] / 1024 / 1024;
-    // $fileSize = getimagesize($tmpPath); // only for image type
+    try {
+        if ($_FILES['sample_file']['name'] !== "") :
+            $fileName = $_FILES['sample_file']['name'];
+            $filePath = $_FILES['sample_file']['tmp_name']; //get the path of the file
+            $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION); // get extension
+            $newFileName = time() . "." . $fileExtension; //change file name
+            $fileSize = $_FILES['sample_file']['size'] / 1024 / 1024;
+            // $fileSize = getimagesize($tmpPath); // only for image type
 
-    // print file info
-    // echo nl2br(
-    //     "Filename: $fileName
-    //     File path: $filePath
-    //     Extension: $fileExtension
-    //     Size: $fileSize"
-    // );
+            // print file info
+            // echo nl2br(
+            //     "Filename: $fileName
+            //     File path: $filePath
+            //     Extension: $fileExtension
+            //     Size: $fileSize"
+            // );
 
-    // unsecure extension validation
-    // if ($fileNameArr[1] == "jpg" || $fileNameArr[1] == "png" || $fileNameArr[1] == "gif") {
-    //     move_uploaded_file($tmpPath, "uploads/$newFileName");
-    // } else {
-    //     echo 'Invalid file type';
-    // }
+            // unsecure extension validation
+            // if ($fileNameArr[1] == "jpg" || $fileNameArr[1] == "png" || $fileNameArr[1] == "gif") {
+            //     move_uploaded_file($tmpPath, "uploads/$newFileName");
+            // } else {
+            //     echo 'Invalid file type';
+            // }
 
-    $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    $mime = finfo_file($finfo, $filePath);
-    if (
-        $mime == 'image/jpeg'
-        || $mime == 'image/png'
-        || $mime == 'image/gif'
-        || $mime == 'application/pdf'
-    ) {
-        if ($fileSize <= 25) {
-            copy($filePath, "uploads_copy/$newFileName"); //not deleting the src file
-            move_uploaded_file($filePath, "uploads/$newFileName");
-        } else {
-            echo 'File size must be within 25MB';
-        }
-    } else {
-        echo 'Invalid format';
-    };
-    finfo_close($finfo);
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $mime = finfo_file($finfo, $filePath);
+            if (
+                $mime == 'image/jpeg'
+                || $mime == 'image/png'
+                || $mime == 'image/gif'
+                || $mime == 'application/pdf'
+            ) {
+                if ($fileSize <= 25) {
+                    copy($filePath, "uploads_copy/$newFileName"); //not deleting the src file
+                    move_uploaded_file($filePath, "uploads/$newFileName");
+                } else {
+                    throw new Exception("File must be within 25MB");
+                }
+            } else {
+                throw new Exception("Wrong file format");
+            };
+            finfo_close($finfo);
+        else : throw new Exception("Please upload a file");
+        endif;
+    } catch (Exception $e) {
+        $error_msg = $e->getMessage();
+    }
 }
 ?>
 
@@ -64,6 +71,7 @@ if (isset($_POST['file_upload'])) {
                 <td><input type="submit" value="Submit" name="file_upload"></td>
             </tr>
         </table>
+        <p style="color:red"><?= $error_msg ?? "" ?></p>
     </form>
 </body>
 
