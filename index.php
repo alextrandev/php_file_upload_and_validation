97399c7ab00f702f03dd1b4999b225a4dd35c134
dk10119 <tranduclong092@gmail.com>
2024-04-11 13:00:55 +0300

    add file size validation, refactor

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 if (isset($_POST['file_upload'])) {
-    $fileName = $_FILES['sample_file']['name']; //get the file name from the FILES superglobal
-    $tmpPath = $_FILES['sample_file']['tmp_name']; //get the path of the file. file will be store in a temp folder when uploaded
-    $fileNameArr = explode('.', $fileName);
-    $newFileName = "image." . $fileNameArr[1]; //change file name
+    $fileName = $_FILES['sample_file']['name'];
+    $filePath = $_FILES['sample_file']['tmp_name']; //get the path of the file
+    $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION); // get extension
+    $newFileName = time() . $fileExtension; //change file name
+    $fileSize = $_FILES['sample_file']['size'] / 1024 / 1024;
+    // $fileSize = getimagesize($tmpPath); // only for image type
 
+    // print file info
     // echo nl2br(
     //     "Filename: $fileName
-    //     Temp path: $tmpPath
-    //     Name: $fileNameArr[0]
-    //     Extension: $fileNameArr[1]"
-    // ); // uploading file info
+    //     File path: $filePath
+    //     Extension: $fileExtension
+    //     Size: $fileSize"
+    // );
 
     // unsecure extension validation
     // if ($fileNameArr[1] == "jpg" || $fileNameArr[1] == "png" || $fileNameArr[1] == "gif") {
@@ -20,12 +23,22 @@ if (isset($_POST['file_upload'])) {
     // }
 
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    $mime = finfo_file($finfo, $tmpPath);
-    if ($mime == 'image/jpeg' || 'image/png' || 'image/gif' || 'application/pdf') {
-        move_uploaded_file($tmpPath, "uploads/$newFileName");
+    $mime = finfo_file($finfo, $filePath);
+    if (
+        $mime == 'image/jpeg'
+        || $mime == 'image/png'
+        || $mime == 'image/gif'
+        || $mime == 'application/pdf'
+    ) {
+        if ($fileSize <= 25) {
+            move_uploaded_file($filePath, "uploads/$newFileName");
+        } else {
+            echo 'File size must be within 25MB';
+        }
     } else {
         echo 'Invalid format';
     };
+    finfo_close($finfo);
 }
 ?>
 
