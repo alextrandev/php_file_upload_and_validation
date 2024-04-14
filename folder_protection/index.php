7dbea408916acd44a7e6c15bc61058c698e0c3e2
dk10119 <tranduclong092@gmail.com>
2024-04-14 11:26:15 +0300

    add sample folder with secret subfolder

---
+++ folder_protection/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Folder protection</title>
+</head>
+
+<body>
+    <h1>Homepage</h1>
+    <p>folder name uploads is not accessible</p>
+
+    <img src="uploads/1712842118.jpeg" alt="sample image">
+    <img src="uploads/1712842121.jpg" alt="sample image">
+    <img src="uploads/1712842125.jpeg" alt="sample image">
+</body>
+
+<style>
+    img {
+        width: 30vw;
+    }
+</style>
+
+</html>
---
+++ folder_protection/uploads/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secret folder</title>
+</head>
+
+<body>
+    <h1>Secret folder</h1>
+    <a href="./1712842118.jpeg">Image 1</a>
+    <a href="./1712842125.jpeg">Image 2</a>
+    <a href="./1712842121.jpg">Image 3</a>
+</body>
+
+</html>
